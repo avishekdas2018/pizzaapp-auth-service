cf7a5485183929d9d7b40c6b07df2102f4a19b2f
Avishek Das <dasavishek2013@gmail.com>
2024-07-27 22:36:27 +0530

    implement global handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
 import express from "express";
+import logger from "./config/logger";
+import { NextFunction, Request, Response } from "express";
+import createHttpError, { HttpError } from "http-errors";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        message: err.message,
+        path: "",
+        location: "",
+      },
+    ],
+  });
 });
 
 export default app;
