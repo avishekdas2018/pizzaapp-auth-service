51c59a06acb1b737e986072aab717c7a17feb228
Avishek Das <dasavishek2013@gmail.com>
2024-07-27 11:17:06 +0530

    add express

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import app from "./app";
+import { Config } from "./config";
+
+const startServer = () => {
+  const PORT = Config.PORT;
+
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+startServer();
