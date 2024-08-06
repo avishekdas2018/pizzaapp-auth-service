d6a744660c2b1c4e339eddc2cc2f89b825154da5
Avishek Das <dasavishek2013@gmail.com>
2024-08-06 11:15:28 +0530

    write multiple test

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
+import "reflect-metadata";
 import express from "express";
 import logger from "./config/logger";
 import { NextFunction, Request, Response } from "express";
 import { HttpError } from "http-errors";
+
+import authRouter from "./routes/auth";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Well come to auth service...");
 });
+
+app.use("/auth", authRouter);
 
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
   logger.error(err.message);
