b94e6bbd882e9cf5e4db225f7164f7d4bf2d7517
Avishek Das <dasavishek2013@gmail.com>
2024-09-21 21:12:19 +0530

    add authentication to admin route

--- tests/tenants/create.spec.ts
+++ tests/tenants/create.spec.ts
@@ -3,17 +3,31 @@ import { AppDataSource } from "../../src/config/data-source";
 import request from "supertest";
 import app from "../../src/app";
 import { Tenant } from "../../src/entity/Tenant";
+import createJWKSMock from "mock-jwks";
+import { Roles } from "../../src/constants";
 
 describe("POST /tenants", () => {
   let connection: DataSource;
+  let jwks: ReturnType<typeof createJWKSMock>;
+  let adminToken: string;
 
   beforeAll(async () => {
+    jwks = createJWKSMock("http://localhost:5501");
     connection = await AppDataSource.initialize();
   });
 
   beforeEach(async () => {
+    jwks.start();
     await connection.dropDatabase();
     await connection.synchronize();
+    adminToken = jwks.token({
+      sub: "1",
+      role: Roles.ADMIN,
+    });
+  });
+
+  afterEach(() => {
+    jwks.stop();
   });
 
   afterAll(async () => {
@@ -27,7 +41,10 @@ describe("POST /tenants", () => {
         address: "123, Main Street, Konnagar",
       };
 
-      const response = await request(app).post("/tenants").send(tenantData);
+      const response = await request(app)
+        .post("/tenants")
+        .set("Cookie", [`accessToken=${adminToken}`])
+        .send(tenantData);
       expect(response.status).toBe(201);
     });
 
@@ -37,13 +54,30 @@ describe("POST /tenants", () => {
         address: "123, Main Street, Konnagar",
       };
 
-      await request(app).post("/tenants").send(tenantData);
+      await request(app)
+        .post("/tenants")
+        .set("Cookie", [`accessToken=${adminToken}`])
+        .send(tenantData);
 
       const tenantRepository = connection.getRepository(Tenant);
       const tenants = await tenantRepository.find();
       expect(tenants).toHaveLength(1);
       expect(tenants[0].name).toBe(tenantData.name);
       expect(tenants[0].address).toBe(tenantData.address);
+    });
+
+    it("should return 401 if user is not authenticated", async () => {
+      const tenantData = {
+        name: "Joy Kali Pizza",
+        address: "123, Main Street, Konnagar",
+      };
+
+      const response = await request(app).post("/tenants").send(tenantData);
+      expect(response.statusCode).toBe(401);
+
+      const tenantRepository = connection.getRepository(Tenant);
+      const tenants = await tenantRepository.find();
+      expect(tenants).toHaveLength(0);
     });
   });
 });
